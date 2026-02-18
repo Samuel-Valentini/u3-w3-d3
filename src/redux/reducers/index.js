19c63cb15c5f6131e267f9e402c9cb9144af52d9
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-18 16:43:17 +0100

    every company is added just one time

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,17 +6,22 @@ const initialState = {
 
 const mainReducer = (currentState = initialState, action) => {
     switch (action.type) {
-        case "ADD_TO_FAVORITES":
-            return {
-                ...currentState,
-                favorite: {
-                    ...currentState.favorite,
-                    companies: [
-                        ...currentState.favorite.companies,
-                        action.payload,
-                    ],
-                },
-            };
+        case "ADD_TO_FAVORITES": {
+            const company = action.payload;
+            if (currentState.favorite.companies.includes(company)) {
+                return currentState;
+            } else
+                return {
+                    ...currentState,
+                    favorite: {
+                        ...currentState.favorite,
+                        companies: [
+                            ...currentState.favorite.companies,
+                            company,
+                        ],
+                    },
+                };
+        }
 
         case "REMOVE_FROM_FAVORITES":
             return {
