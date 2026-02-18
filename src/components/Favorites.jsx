07dcf0e9148fa858afbf26a1054c79becadd946b
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-18 17:30:51 +0100

    key problem solved

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -16,22 +16,22 @@ const Favorites = () => {
             <h1 className="text-danger text-center">Favorites</h1>
             {[...new Set(arrayOfFavorites)].map((company) => {
                 return (
-                    <>
-                        <div className="d-flex justify-content-between w-50 mx-auto my-2">
-                            <h3 key={company}>
-                                <Link to={`/${company}`}>{company}</Link>
-                            </h3>
-                            <Button
-                                onClick={() => {
-                                    dispatch({
-                                        type: "REMOVE_FROM_FAVORITES",
-                                        payload: company,
-                                    });
-                                }}>
-                                Remove
-                            </Button>
-                        </div>
-                    </>
+                    <div
+                        key={company}
+                        className="d-flex justify-content-between w-50 mx-auto my-2">
+                        <h3>
+                            <Link to={`/${company}`}>{company}</Link>
+                        </h3>
+                        <Button
+                            onClick={() => {
+                                dispatch({
+                                    type: "REMOVE_FROM_FAVORITES",
+                                    payload: company,
+                                });
+                            }}>
+                            Remove
+                        </Button>
+                    </div>
                 );
             })}
         </div>
