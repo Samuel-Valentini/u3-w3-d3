4b90ff4f7519b22c1172d3f80fd6fac7935962a2
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-18 16:19:26 +0100

    link for every company added

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
     const arrayOfFavorites = useSelector((curr) => curr.favorite.companies);
@@ -7,7 +8,11 @@ const Favorites = () => {
         <div>
             <h1 className="text-danger">Favorites</h1>
             {[...new Set(arrayOfFavorites)].map((company, i) => {
-                return <h3 key={i}>{company}</h3>;
+                return (
+                    <h3 key={company}>
+                        <Link to={`/${company}`}>{company}</Link>
+                    </h3>
+                );
             })}
         </div>
     );
